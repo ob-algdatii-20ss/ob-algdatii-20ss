db0d42947ad126b8f9f5f0e5b69f16d1a2ca01bb
Oliver Braun <ob@cs.hm.edu>
2020-03-23 09:38:26 +0100

    Stand Vorlesung

--- graph/adjMat.go
+++ graph/adjMat.go
@@ -14,3 +14,25 @@ func NewGraphAdjMat(nodes int) AdjMat {
 func (g AdjMat) AddEdge(from Node, to Node) {
 	g[from][to] = true
 }
+
+func (g AdjMat) Edges() []Edge {
+	es := make([]Edge, 0)
+
+	for i, col := range g {
+		if i == 0 {
+			continue
+		}
+
+		for j, edge := range col {
+			if j == 0 {
+				continue
+			}
+
+			if edge {
+				es = append(es, Edge{Node(i), Node(j)})
+			}
+		}
+	}
+
+	return es
+}
--- graph/graph.go
+++ graph/graph.go
@@ -2,6 +2,12 @@ package graph
 
 type Node int
 
+type Edge struct {
+	from Node
+	to   Node
+}
+
 type Graph interface {
 	AddEdge(from Node, to Node)
+	Edges() []Edge
 }
