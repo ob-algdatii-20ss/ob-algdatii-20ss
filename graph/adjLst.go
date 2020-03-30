f255495499f206e87191e50dbffa5d608f10cd8d
Oliver Braun <ob@cs.hm.edu>
2020-03-30 10:34:51 +0200

    add IsAcyclic to AdjLst

--- graph/adjLst.go
+++ graph/adjLst.go
@@ -60,3 +60,63 @@ func (g AdjLst) ToDot() dot.Graph {
 
 	return *dg
 }
+
+// O(|V| + |E|)
+func (g AdjLst) IsAcyclic() bool {
+	// Eingangsgrade berechnen
+	indeg := make(map[Node]int)
+
+	// O(|V| + |E|)
+	for _, elem := range g {
+		for current := elem; current != nil; current = current.next {
+			indeg[current.value]++
+		}
+	}
+
+	// übernehme alle Knoten mit Eingangsgrad 0 in "stack"
+	indegZero := make([]Node, 0)
+
+	// O(|V|)
+	for i := range g {
+		if i == 0 {
+			continue
+		}
+
+		n := Node(i)
+
+		if indeg[n] == 0 {
+			indegZero = append(indegZero, n)
+		}
+	}
+
+	// topologische Sortierung berechnen
+	seqNo := 0
+	ord := make(map[Node]int)
+
+	// O(|V| + |E|)
+	// maximal durch alle Knoten
+	// jeder Pfeil insgesamt über alle inneren Schleifen je einmal
+	for len(indegZero) > 0 {
+		// nehme den nächsten vom Stack
+		lastIdx := len(indegZero) - 1
+		v := indegZero[lastIdx]
+		indegZero = indegZero[:lastIdx]
+
+		seqNo++
+		ord[v] = seqNo
+
+		// dekrementiere Eingangsgrade aller Knoten die Endknoten
+		// eines Pfeiles sind, von dem v der Anfangsknoten ist
+		for current := g[v]; current != nil; current = current.next {
+			indeg[current.value]--
+
+			if indeg[current.value] == 0 {
+				indegZero = append(indegZero, current.value)
+			}
+		}
+	}
+
+	// wenn jeder Knoten eine laufende Nummer bekommen hat,
+	// ist der Graph azyklisch.
+	return seqNo == len(g)-1
+}
