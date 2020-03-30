5b586d0a406014e6da9b1c4215735f85fd4ec27a
Oliver Braun <ob@cs.hm.edu>
2020-03-30 10:36:09 +0200

    use ToDot and IsAcyclic in main

--- main.go
+++ main.go
@@ -8,16 +8,21 @@ import (
 
 func main() {
 	var g graph.Graph = graph.NewGraphAdjLst(9,
-		&graph.Edge{1, 2},
-		&graph.Edge{1, 3},
-		&graph.Edge{1, 7},
-		&graph.Edge{4, 6},
-		&graph.Edge{5, 4},
-		&graph.Edge{6, 1},
-		&graph.Edge{6, 5},
-		&graph.Edge{6, 6},
-		&graph.Edge{7, 5},
-		&graph.Edge{9, 8})
+		&graph.Edge{From: 1, To: 2},
+		&graph.Edge{From: 1, To: 3},
+		&graph.Edge{From: 1, To: 7},
+		&graph.Edge{From: 4, To: 6},
+		&graph.Edge{From: 5, To: 4},
+		&graph.Edge{From: 6, To: 1},
+		&graph.Edge{From: 6, To: 5},
+		&graph.Edge{From: 6, To: 6},
+		&graph.Edge{From: 7, To: 5},
+		&graph.Edge{From: 9, To: 8})
 
 	fmt.Println(g.ToDot().String())
+	if g.IsAcyclic() {
+		fmt.Println("# ist zyklenfrei")
+	} else {
+		fmt.Println("# ist nicht zyklenfrei")
+	}
 }
